f69fb86dc70f189732ac37992cdce2dcca374b0b
Nicolas H <hubnersantos@aol.com>
2024-12-10 23:22:52 -0300

    Feat Improve Function
    Co-authored-by: Israel Santos <DevIsraelSantos@users.noreply.github.com>

--- rpa_sus/functions/dbf/send_elastic_dbf_pa.py
+++ rpa_sus/functions/dbf/send_elastic_dbf_pa.py
@@ -6,6 +6,7 @@ import os
 import time
 
 from configs.database import ELASTICSEARCH_HOST, ES_INDEX_NAME_PREFIX, CHUNK_SIZE, MAX_RETRIES, RETRY_DELAY, NUM_PROCESSES, ELASTIC_USERNAME, ELASTIC_PASSWORD
+from configs.constants import COLUMNS_TO_WATCH, state_codes
 
 
 # DBF directory path
@@ -25,33 +26,47 @@ INT_CHUNK_SIZE = int(CHUNK_SIZE)
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
-COLUMNS_TO_WATCH = ["PA_CODUNI",
-                    "PA_UFMUN",
-                    "PA_CNPJCPF",
-                    "PA_CNPJMNT",
-                    "PA_CMP",
-                    "PA_PROC_ID",
-                    "PA_TPFIN",
-                    "PA_SUBFIN",
-                    "PA_AUTORIZ",
-                    "PA_CIDPRI",
-                    "PA_CIDSEC",
-                    "PA_CATEND",
-                    "PA_QTDPRO",
-                    "PA_QTDAPR",
-                    "PA_VALPRO",
-                    "PA_VALAPR",
-                    ]
+
+# Precomputed mapping for state codes
+state_code_to_name = {str(code).zfill(
+    2): name for name, code in state_codes.items()}
 
 
 def prepare_es_doc(record, index_name, fields_to_include=COLUMNS_TO_WATCH):
-    # Filter fields based on the columns to watch
     if fields_to_include:
         record = {k: v for k, v in record.items() if k in fields_to_include}
+    uf_code = str(record.get("PA_UFMUN", ""))[:2]
+    record["PA_UFMUN"] = state_code_to_name.get(uf_code, "Unknown")
     return {
         '_index': index_name,
         '_source': {k: str(v) if v is not None else None for k, v in record.items()},
     }
+
+
+def ensure_index_exists(index_name):
+    if not es.indices.exists(index=index_name):
+        es.indices.create(index=index_name, body={
+            "mappings": {
+                "properties": {
+                    "PA_CODUNI": {"type": "keyword"},
+                    "PA_UFMUN": {"type": "keyword"},
+                    "PA_CNPJCPF": {"type": "keyword"},
+                    "PA_CNPJMNT": {"type": "keyword"},
+                    "PA_CMP": {"type": "keyword"},
+                    "PA_PROC_ID": {"type": "integer"},
+                    "PA_TPFIN": {"type": "keyword"},
+                    "PA_SUBFIN": {"type": "integer"},
+                    "PA_AUTORIZ": {"type": "keyword"},
+                    "PA_CIDPRI": {"type": "keyword"},
+                    "PA_CIDSEC": {"type": "keyword"},
+                    "PA_CATEND": {"type": "keyword"},
+                    "PA_QTDPRO": {"type": "integer"},
+                    "PA_QTDAPR": {"type": "integer"},
+                    "PA_VALPRO": {"type": "keyword"},
+                    "PA_VALAPR": {"type": "keyword"},
+                }
+            }
+        })
 
 
 def read_in_batches(file_path, chunk_size):
@@ -70,37 +85,13 @@ def read_in_batches(file_path, chunk_size):
 # # Function to process a chunk of data and send it to Elasticsearch
 def process_chunk(data_chunk, index_name):
     try:
-        if not es.indices.exists(index_name):
-            es.indices.create(index=index_name, body={
-                "mappings": {
-                    "properties": {
-                        "PA_CODUNI": {"type": "keyword"},
-                        "PA_UFMUN": {"type": "keyword"},
-                        "PA_CNPJCPF": {"type": "keyword"},
-                        "PA_CNPJMNT": {"type": "keyword"},
-                        "PA_CMP": {"type": "keyword"},
-                        "PA_PROC_ID": {"type": "integer"},
-                        "PA_TPFIN": {"type": "keyword"},
-                        "PA_SUBFIN": {"type": "integer"},
-                        "PA_AUTORIZ": {"type": "keyword"},
-                        "PA_CIDPRI": {"type": "keyword"},
-                        "PA_CIDSEC": {"type": "keyword"},
-                        "PA_CATEND": {"type": "keyword"},
-                        "PA_QTDPRO": {"type": "integer"},
-                        "PA_QTDAPR": {"type": "integer"},
-                        "PA_VALPRO": {"type": "keyword"},
-                        "PA_VALAPR": {"type": "keyword"},
-                    }
-                }
-            })
 
         actions = (prepare_es_doc(record, index_name)
                    for record in data_chunk if record)
 
         for attempt in range(MAX_RETRIES):
             try:
-                # Ensure chunk_size is passed as a keyword argument
-                for ok, _ in helpers.streaming_bulk(es, actions, chunk_size=INT_CHUNK_SIZE):
+                for ok, _ in helpers.streaming_bulk(client=es, actions=actions, chunk_size=INT_CHUNK_SIZE):
                     if not ok:
                         logging.error("A document failed to index.")
                 break
@@ -128,6 +119,8 @@ def parallel_bulk_index(dbf_directory):
             index_name = f"{ES_INDEX_NAME_PREFIX}{os.path.splitext(dbf_file)[0]}".lower(
             ).replace(" ", "_").replace("-", "_")
             dbf_file_path = os.path.join(dbf_directory, dbf_file)
+            # Create the index if it doesn't exist
+            ensure_index_exists(index_name)
             try:
                 logging.info(f"Reading DBF file: {dbf_file}")
                 for data_chunk in read_in_batches(dbf_file_path, INT_CHUNK_SIZE):
