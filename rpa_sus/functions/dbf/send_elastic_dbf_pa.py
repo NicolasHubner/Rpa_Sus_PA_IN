6e7bc967331fe1ba32de6387d9eb1ec48a9601ba
Nicolas H <hubnersantos@aol.com>
2025-01-14 21:59:19 -0300

    Feat Add new features

--- rpa_sus/functions/dbf/send_elastic_dbf_pa.py
+++ rpa_sus/functions/dbf/send_elastic_dbf_pa.py
@@ -1,6 +1,7 @@
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from datetime import datetime
 import datetime
+from http.client import LineTooLong
 from dbfread import DBF
 from elasticsearch import Elasticsearch, helpers
 import logging
@@ -12,15 +13,16 @@ from configs.constants import CARATEND_CODES, COLUMNS_TO_WATCH, state_codes, FIN
 
 
 # DBF directory path
-dbf_directory = './data/pa_acre'  # Specify the directory containing DBF files
+# dbf_directory = './data/pa_acre'  # Specify the directory containing DBF files
 
-# dbf_directory = '/mnt/volume_nyc1_01/nicolas/alagoas/PA_ACIMA_2008'
+dbf_directory = '/mnt/volume_nyc1_01/nicolas/alagoas/PA_ACIMA_2008'
 
 # Create Elasticsearch client with comprehensive error handling
 try:
     es_client = Elasticsearch(
         [ELASTICSEARCH_HOST],
         basic_auth=(ELASTIC_USERNAME, ELASTIC_PASSWORD),
+        max_retries=MAX_RETRIES,
     ).options(request_timeout=60)
 except Exception as connection_error:
     print(f"Elasticsearch connection failed: {connection_error}")
@@ -98,29 +100,29 @@ def prepare_es_doc(record, index_name, fields_to_include=COLUMNS_TO_WATCH):
     cleaned_record["PA_UFMUN"] = get_mapped_value(uf_code, code_to_state)
 
     # # Step 4: Handle PA_CMP conversion to yyyyMM format
-    # pa_cmp = cleaned_record.get("PA_CMP", "")
-    # if pa_cmp:
-    #     cleaned_record["PA_CMP"] = handle_date_conversion(pa_cmp)
-    #     if cleaned_record["PA_CMP"]:
-    #         cleaned_record["@timestamp"] = cleaned_record["PA_CMP"]
+    pa_cmp = cleaned_record.get("PA_CMP", "")
+    if pa_cmp:
+        cleaned_record["PA_CMP"] = handle_date_conversion(pa_cmp)
+        if cleaned_record["PA_CMP"]:
+            cleaned_record["@timestamp"] = cleaned_record["PA_CMP"]
 
-    # # Step 5: Handle PA_TPFIN conversion to financial code
-    # pa_tpfin = cleaned_record.get("PA_TPFIN", "")
-    # if pa_tpfin:
-    #     cleaned_record["PA_TPFIN"] = get_mapped_value(
-    #         pa_tpfin, financ_codes_to_name)
+    # Step 5: Handle PA_TPFIN conversion to financial code
+    pa_tpfin = cleaned_record.get("PA_TPFIN", "")
+    if pa_tpfin:
+        cleaned_record["PA_TPFIN"] = get_mapped_value(
+            pa_tpfin, financ_codes_to_name)
 
-    # # Step 6: Handle PA_SUBFIN conversion to faectp code
-    # pa_subfin = cleaned_record.get("PA_SUBFIN", "")
-    # if pa_subfin:
-    #     pa_subfin = pa_subfin.lstrip('0')
-    #     cleaned_record["PA_SUBFIN"] = get_mapped_value(pa_subfin, FAECTP_CODES)
+    # Step 6: Handle PA_SUBFIN conversion to faectp code
+    pa_subfin = cleaned_record.get("PA_SUBFIN", "")
+    if pa_subfin:
+        pa_subfin = pa_subfin.lstrip('0')
+        cleaned_record["PA_SUBFIN"] = get_mapped_value(pa_subfin, FAECTP_CODES)
 
-    # # Step 7: Handle PA_CARATEND conversion to caratend code
-    # pa_caratend = cleaned_record.get("PA_CATEND", "")
-    # if pa_caratend:
-    #     cleaned_record["PA_CATEND"] = get_mapped_value(
-    #         pa_caratend, CARATEND_CODES)
+    # Step 7: Handle PA_CARATEND conversion to caratend code
+    pa_caratend = cleaned_record.get("PA_CATEND", "")
+    if pa_caratend:
+        cleaned_record["PA_CATEND"] = get_mapped_value(
+            pa_caratend, CARATEND_CODES)
 
     # Step 8: Prepare the final document for Elasticsearch
     return {
@@ -191,6 +193,9 @@ def read_in_batches(dbf_file_path, batch_size):
 processed_records_count = 0
 
 
+MAX_BACKOFF = 60  # Maximum backoff time in seconds
+
+
 def process_chunk(data_chunk, index_name):
     """Process a chunk of data and send it to Elasticsearch."""
     try:
@@ -199,51 +204,46 @@ def process_chunk(data_chunk, index_name):
             for record in data_chunk if record
         )
 
-        record_count = 0  # Counter to track the number of records processed
-        failed_documents = 0  # Track the number of failed documents
+        record_count = 0
+        failed_documents = 0
 
-        # Attempting bulk indexing with retries
         for attempt in range(MAX_RETRIES):
             try:
-                # Perform bulk indexing and check if documents are indexed successfully
                 for ok, response in helpers.streaming_bulk(
-                    client=es_client, actions=actions, chunk_size=INT_CHUNK_SIZE
+                    client=es_client, actions=actions, chunk_size=INT_CHUNK_SIZE, max_retries=3
                 ):
                     if not ok:
                         failed_documents += 1
-                        # Log the error but avoid printing entire response, log only the error reason
                         error_message = response.get('error', {}).get(
                             'reason', 'Unknown error')
                         logging.error(
                             f"Failed to index document: {error_message}")
                     else:
-                        record_count += 1  # Increment counter for each successful record
+                        record_count += 1
 
-                # If no documents failed, exit the loop
                 if failed_documents == 0:
                     break
-            except Exception as e:
-                # Log the error and retry if necessary
+            except LineTooLong as e:
                 logging.error(
-                    f"Error during bulk indexing to {index_name} (attempt {attempt + 1}/{MAX_RETRIES}): {str(e)}")
-                # Exponential backoff for retry
-                time.sleep(RETRY_DELAY * (2 ** attempt))
+                    f"LineTooLong error: {e}. Try reducing chunk size.")
+                break
+            except Exception as e:
+                backoff = min(RETRY_DELAY * (2 ** attempt), MAX_BACKOFF)
+                logging.error(
+                    f"Error during bulk indexing (attempt {attempt + 1}/{MAX_RETRIES}): {e}")
+                time.sleep(backoff)
         else:
-            # If all attempts fail, log the failure
             logging.error(
                 f"Failed to index chunk after {MAX_RETRIES} attempts.")
 
         if failed_documents > 0:
-            logging.error(
-                f"{failed_documents} document(s) failed to index in total.")
+            logging.error(f"{failed_documents} document(s) failed to index.")
 
-        return record_count  # Return the count of processed records in this chunk
+        return record_count
 
     except Exception as e:
-        # Catching and logging any exceptions during the chunk processing
-        logging.error(
-            f"Error processing chunk for index {index_name}: {str(e)}")
-        return 0  # If an error occurs, return 0 to ensure no records are counted
+        logging.error(f"Error processing chunk for index {index_name}: {e}")
+        return 0
 
 
 def parallel_bulk_index(dbf_directory):
